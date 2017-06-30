4e51efe4684cd44a27a4bb9bf1e16b4e6cceccf1
juho0719 <juho0719@naver.com>
2017-06-30 17:07:28 +0900

    updates

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blogs/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blogs/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogs/post_detail.html', {'post': post})
